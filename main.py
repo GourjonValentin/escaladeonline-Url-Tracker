9cfecdb041a0f9fe6dc2a29f6ec0d4e820f5541c
Valentin <valentin.gourjon@efrei.net>
2023-04-25 13:06:01 +0200

    Voila

--- main.py
+++ main.py
@@ -1,12 +1,86 @@
 import requests
 from tqdm import tqdm
+import json
+import os
 
-
-RANGE_MIN = 11500
-RANGE_MAX = 11600
 BASE_URL = "https://escalade.online/ws/app/resultatJson/"
 
+def getRouteFromId(id, groupe):
+    for route in groupe['etapes']:
+        if route['id'] == id:
+            return route
+
+def removeSlash(str):
+    return str.replace("/", "-")
+
+def extractData(id, logs = False):
+    with open(f"data/{id}.json") as f:
+        data = json.load(f)
+
+    print(data['nomCompetition'])
+    style = "<style>table, th, td {border: 1px solid black; border-collapse: collapse; text-align: center;}</style>"
+    strTable = f"<!DOCTYPE html><head><meta http-equiv=\"refresh\" content=\"5\"><body><html>{style}<h1>{data['nomCompetition']}</h1><br/>"
+    strTable += f"<h2>Lieu : {data['lieu']}</h2><br/>"
+    strTable += f"<h3><a href='https://escalade.online/resultat.html?id={id}'>Lien vers les résultats officiels</a></h3><br/>"
+
+
+    for groupe in data['groupes']:
+        if groupe['resultats'] != []: # Ne prends pas les regroupements où il n'y a pas de classement
+
+            groupeHeader = f"{groupe['categorie']} {groupe['sexe']}" # Header du regroupement
+
+            strSubTable = f"<h2>{groupeHeader}</h2><br/>"
+
+            if logs:
+                print("\n")
+
+                print(groupeHeader)
+
+            strSubTable += "<table><tr><th>Classement</th><th>Competiteur</th><th>Club</th>"
+
+            for route in groupe['etapes']:
+                strSubTable += f"<th>Voie {route['nom']}  <br/>{route['couleur']}</th>"
+
+            strSubTable += "</tr>"
+
+            for competiteur in groupe['resultats']:
+                if logs:
+                    print(f"{competiteur['nom']} {competiteur['prenom']}")  # Header du compétiteur
+
+                strRow = f"<tr><td>{competiteur['classement']}</td><td>{competiteur['nom']} {competiteur['prenom']}</td><td>{competiteur['nomClub']}</td>"
+
+                # Résultats du compétiteur
+                for etape in competiteur['etapes']:
+                    id = etape['idEtape']
+                    route = getRouteFromId(id, groupe)
+
+                    if logs:
+                        print(f"Voie {route['nom']} {route['couleur']} : {etape['resultat']}")
+
+                    strRow += f"<td>{etape['resultat']}</td>"
+
+                # Classement du compétiteur
+                if logs:
+                    print(f"Classement : {competiteur['classement']}")
+                    print()
+
+                strRow += f"</tr>"
+                strSubTable += strRow
+
+
+
+            strSubTable += "</table><br/>"
+            strTable += strSubTable
+
+    strTable += "</body></html>"
+    file = f"html/{removeSlash(data['nomCompetition'])}.html"
+    with open(file, 'w') as f:
+        f.write(strTable)
+
 if __name__ == "__main__":
+
+    RANGE_MIN = int(input("Borne inférieure (environ 11550 en avril 2023) : "))
+    RANGE_MAX = int(input("Borne supérieure: (environ 11600 en avril 2023) : "))
 
     comps = {}
     for i in tqdm(range(RANGE_MIN, RANGE_MAX)):
@@ -15,12 +89,46 @@ if __name__ == "__main__":
         if r.text != "":
             data = r.json()
             comps[i] = data
-            print(f"Found competition: {data['nomCompetition']} at url https://escalade.online/resultat.html?id={i}")
+            print(f"\nFound competition: {data['nomCompetition']} at url https://escalade.online/resultat.html?id={i}")
 
     print("Requests done.")
 
     print(f"Found {len(list(comps))} competitions:")
+
+    try:
+        os.mkdir("data")
+    except:
+        pass
+
     for i in comps:
         print(f"{comps[i]['nomCompetition']} : https://escalade.online/resultat.html?id={i}")
+        with open(f"data/{i}.json", 'w') as j:
+            json.dump(comps[i], j)
+
+    print("Voulez-vous extraire les données ?")
+    print("1. Oui")
+    print("2. Non")
+    choice = int(input("Choix: "))
+    if choice == 1:
+
+        data_files = os.listdir("data/")
+        try:
+            os.mkdir("html")
+        except:
+            pass
 
 
+        for id in data_files:
+            try:
+                extractData(id.replace(".json", ""), False)
+            except:
+                print(f"Erreur lors de l'extraction de {id}")
+                continue
+        print()
+        print("Les données ont été extraites dans le dossier html.")
+        print()
+
+    else:
+        print("Fin du programme.")
+
+    input("Press enter to exit.")
